f35c228577d0b6c7b7d66f412cef9af7e492bd46
Flavio Nogueira <flavionogueirabarros@gmail.com>
2025-03-24 16:51:22 -0300

    add clojure as main language

--- init.lua
+++ init.lua
@@ -398,7 +398,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'html', 'svelte', 'dart', 'yaml' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'html', 'svelte', 'dart', 'yaml', 'fennel', 'clojure', 'css', 'markdown' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
